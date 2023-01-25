c0f0b2bc09ac32f1590bc2624ede6e5a5c388ee7
leeclaire156 <lee.claire156@gmail.com>
2023-01-25 02:10:07 -0500

    Adds Manager tests

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,86 @@
 const Manager = require('../lib/Manager');
+
+// describe() houses the testing units
+// Each unit invoked through it("name", ()=>{testing methods}) or test("name", ()=>{testing methods})
+describe('Manager', () => {
+
+    // Add change 2nd value (id) to "rr18152" if you are using a letter AND number based identification system
+    const manager = new Manager("Sophia-Grace Anderson", 191516, "sop191516@yahoo.co.uk", 101);
+
+    // Test set 1: names
+    it("should set the name as 'Sophia-Grace Anderson'", () => {
+        expect(manager.name).toBe("Sophia-Grace Anderson");
+    })
+
+    it("should return name", () => {
+        expect(manager.getName()).toBe("Sophia-Grace Anderson");
+    })
+
+    // // Test set 2a: ID - number only based ID system
+    it("should set the ID as 191516", () => {
+        expect(manager.id).toBe(191516);
+        expect(typeof (manager.id)).toBe("number");
+    })
+
+    it("should return id", () => {
+        expect(manager.getId()).toBe(191516);
+        expect(typeof (manager.getId())).toBe("number");
+    })
+
+    // // Test set 2b: ID - with non-capitalized letters based ID system
+    // it("should set the ID as 'ss191516'", () => {
+    //     expect(manager.id).toBe("ss191516");
+    //     expect(typeof (manager.id)).toBe("string");
+    // })
+
+    // it("should return id", () => {
+    //     expect(manager.getId()).toBe("ss191516");
+    //     expect(typeof (manager.getId())).toBe("string");
+    // })
+
+    // // Test set 2c: ID - with capitalized letters based ID system
+    // it("should set the ID as 'SS191516'", () => {
+    //     expect(manager.id).toBe("SS191516");
+    //     expect(typeof (manager.id)).toBe("string");
+    // })
+
+    // it("should return id", () => {
+    //     expect(manager.getId()).toBe("SS191516");
+    //     expect(typeof (manager.getId())).toBe("string");
+    // })
+
+    // Test set 3: Email
+    it("should set an email", () => {
+        expect(manager.email).toBe("sop191516@yahoo.co.uk");
+        expect(manager.email).toMatch(/@/);
+        expect(manager.email).toMatch(".")
+    })
+
+
+    it("should return email", () => {
+        expect(manager.getEmail()).toBe("sop191516@yahoo.co.uk");
+        expect(manager.getEmail()).toMatch(/@/);
+        expect(manager.getEmail()).toMatch(".")
+    })
+
+
+    // Test set 4: role
+    it("should set role as 'Manager'", () => {
+        expect(manager.role).toBe("Manager");
+    })
+
+    it("should return role", () => {
+        expect(manager.getRole()).toBe("Manager");
+    })
+
+    // Test set 5: Office Room Number
+    it("should set office room number", () => {
+        expect(manager.officeNumber).toBe(101);
+        expect(typeof (manager.id)).toBe("number");
+    })
+
+    it("should return office room number", () => {
+        expect(manager.getOfficeNumber()).toBe(101);
+        expect(typeof (manager.getId())).toBe("number");
+    })
+});
